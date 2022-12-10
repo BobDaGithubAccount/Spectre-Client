4b6d6f027708b32433b20e481f5c262ca8144c15
BobDaGithubAccount <62474433+BobDaGithubAccount@users.noreply.github.com>
2022-12-10 09:32:30 +0000

    Configured dragon model

--- src/objects/Dragon.java
+++ src/objects/Dragon.java
@@ -15,19 +15,19 @@ public class Dragon extends GameObject {
 		super(name, entity);
 		TexturedModel model = entity.getModel();
 		ModelTexture t = model.getTexture();
-		t.setShineDamper(10f);
-		t.setReflectivity(10f);
+		t.setShineDamper(1f);
+		t.setReflectivity(1f);
 		model.setTexture(t);
 		entity.setModel(model);
 	}
 
 	@Override
 	public void run() {
-//		Entity entity = this.getEntity();
-//		Vector3f rotation = entity.getRotation();
+		Entity entity = this.getEntity();
+		Vector3f rotation = entity.getRotation();
 //		rotation.setX(rotation.getX()+1);
-//		entity.setRotation(rotation);
-//		this.setEntity(entity);
+		entity.setRotation(rotation);
+		this.setEntity(entity);
 	}
 
 }
