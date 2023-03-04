3f7257570b59030a8b3f17c188bebb51893e2301
BobDaGithubAccount <62474433+BobDaGithubAccount@users.noreply.github.com>
2023-03-04 19:03:22 +0000

    a
    a

--- src/renderEngine/sourceEngineCompatibility/ValveMapFormatLoader.java
+++ src/renderEngine/sourceEngineCompatibility/ValveMapFormatLoader.java
@@ -30,7 +30,7 @@ public class ValveMapFormatLoader {
     public static final int HEADER_LUMPS = 64;
     public static final int HEADER_SIZE = 1036;
     public static final int MAX_LUMPFILES = 128;
-	public static List<Lump> lumps = new ArrayList<Lump>(HEADER_LUMPS);
+	public static List<Lump> lumps = new ArrayList<Lump>(HEADER_LUMPS);public static Lump getLump(LumpType type){return lumps.get(type.getIndex());}
     
 	private static void load(String fileName) throws Exception {
 		Logger.log("Attempting to load source map " + fileName);
@@ -63,7 +63,106 @@ public class ValveMapFormatLoader {
 	}
 	
 	private static void loadGameLumps() throws Exception {
-		
+//        Lump lump = getLump(LumpType.LUMP_GAME_LUMP);
+//        DataReader in = DataReaders.forByteBuffer(lump.getBuffer());
+//
+//        // hack for Vindictus
+//        if (version == 20 && bo == ByteOrder.LITTLE_ENDIAN
+//                && checkInvalidHeaders(in, false)
+//                && !checkInvalidHeaders(in, true)) {
+//            L.finer("Found Vindictus game lump header");
+//            appId = VINDICTUS;
+//        }
+//
+//        int glumps = in.readInt();
+//
+//        for (int i = 0; i < glumps; i++) {
+//            int ofs, len, flags, vers, fourCC;
+//
+//            if (appId == DARK_MESSIAH) {
+//                in.readInt(); // unknown
+//            }
+//
+//            fourCC = in.readInt();
+//
+//            // Vindictus uses integers rather than unsigned shorts
+//            if (appId == VINDICTUS) {
+//                flags = in.readInt();
+//                vers = in.readInt();
+//            } else {
+//                flags = in.readUnsignedShort();
+//                vers = in.readUnsignedShort();
+//            }
+//
+//            ofs = in.readInt();
+//            len = in.readInt();
+//
+//            if (flags == 1) {
+//                // game lump is compressed and "len" contains the uncompressed
+//                // size, so use next entry offset to determine compressed size
+//                in.seek(8, CURRENT);
+//                int nextOfs = in.readInt();
+//                if (nextOfs == 0) {
+//                    // no next entry, assume end of game lump
+//                    nextOfs = lump.getOffset() + lump.getLength();
+//                }
+//                len = nextOfs - ofs;
+//                in.seek(-12, CURRENT);
+//            }
+//
+//            // Offset is relative to the beginning of the BSP file,
+//            // not to the game lump.
+//            // FIXME: this isn't the case for the console version of Portal 2,
+//            // is there a better way to detect this?
+//            if (ofs - lump.getOffset() > 0) {
+//                ofs -= lump.getOffset();
+//            }
+//
+//            String glName = StringMacroUtils.unmakeID(fourCC);
+//
+//            // give dummy entries more useful names
+//            if (glName.trim().isEmpty()) {
+//                glName = "<dummy>";
+//            }
+//
+//            // fix invalid offsets
+//            if (ofs > lump.getLength()) {
+//                int ofsOld = ofs;
+//                ofs = lump.getLength();
+//                len = 0;
+//                L.log(Level.WARNING, "Invalid game lump offset {0} in {1}, assuming {2}",
+//                        new Object[]{ofsOld, glName, ofs});
+//            } else if (ofs < 0) {
+//                int ofsOld = ofs;
+//                ofs = 0;
+//                len = 0;
+//                L.log(Level.WARNING, "Negative game lump offset {0} in {1}, assuming {2}",
+//                        new Object[]{ofsOld, glName, ofs});
+//            }
+//
+//            // fix invalid lengths
+//            if (ofs + len > lump.getLength()) {
+//                int lenOld = len;
+//                len = lump.getLength() - ofs;
+//                L.log(Level.WARNING, "Invalid game lump length {0} in {1}, assuming {2}",
+//                        new Object[]{lenOld, glName, len});
+//            } else if (len < 0) {
+//                int lenOld = len;
+//                len = 0;
+//                L.log(Level.WARNING, "Negative game lump length {0} in {1}, assuming {2}",
+//                        new Object[]{lenOld, glName, len});
+//            }
+//
+//            GameLump gl = new GameLump();
+//            gl.setBuffer(ByteBufferUtils.getSlice(lump.getBuffer(), ofs, len));
+//            gl.setOffset(ofs);
+//            gl.setFourCC(fourCC);
+//            gl.setFlags(flags);
+//            gl.setVersion(vers);
+//            gameLumps.add(gl);
+//        }
+//
+//        L.log(Level.FINE, "Game lumps: {0}", glumps);
 	}
 	
 	private static ByteBuffer createBuffer(File file, boolean memoryMapping) throws Exception {
